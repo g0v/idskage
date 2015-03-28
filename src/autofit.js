0eab412a9eb3594db9c099e1475fdec27ab74006
yap cheah shen <yapcheahshen@gmail.com>
2015-03-28 23:00:07 +0800

    Update autofit.js

--- src/autofit.js
+++ src/autofit.js
@@ -5,23 +5,25 @@ yapcheahshen@gmail.com at MOEDICT 萌典松 2015/3/28
 
 */
 
-
+  //返回組字符所需的部件
   var getOperandByIDC=function(c) {	
-    if (c==0x2ff2||c==0x2ff3) return 3; 
-    else if (c>=0x2ff0 && c<=0x2fff) return 2; 
+    if (c==0x2ff2||c==0x2ff3) return 3;  //三元組字符
+    else if (c>=0x2ff0 && c<=0x2fff) return 2; //二元組字符
     else return 0;
   }
-  
+  //全字框, 以小數表示。
   var fullframe=function(){
     return {x1:0,y1:0,x2:1,y2:1} 
   };
   
-  //TODO others IDC, read matrix from 
+ //根據組字符，產生字框，part 為第幾個部件。
+ //sorry AU, 這是很naive 的implementation
   var framebypart=function (idc,frame,part) {
       var f={};
   	  switch (idc) {
   	  	  case 0x2ff0: //⿰
   	  	    if (0==part) {
+  	  	    	
   	  	    	f.x1=frame.x1;f.y1=frame.y1;
   	  	    	f.x2=(frame.x2-frame.x1)/2;f.y2=frame.y2;
   	  	    } else {
@@ -41,17 +43,19 @@ yapcheahshen@gmail.com at MOEDICT 萌典松 2015/3/28
   	  }
   	  return f;
   }
+  
+  //可遞迴的算框
   var fitparts=function(parent,frame) {
 	var idc=parent["ch"].charCodeAt(0);
 	var operand=getOperandByIDC(idc);
 	var i=1;
   	while (operand>0) {
-  		f=framebypart(idc,frame,i-1);
-  		var child=parent["p"+i];
-  		op=isIDC(child["ch"].charCodeAt(0));
-  		if (op>0) fitparts(child, f);
-          else child.frame=f;
-  		i++;operand--;
+  	  f=framebypart(idc,frame,i-1);
+  	  var child=parent["p"+i];  //中間代號
+  	  op=isIDC(child["ch"].charCodeAt(0));
+  	  if (op>0) fitparts(child, f);//又踫到 IDC，遞迴
+  	  else child.frame=f;
+  	  i++;operand--;
   	}
   }
   var idstree={};//a tree to hold IDS
@@ -70,25 +74,25 @@ yapcheahshen@gmail.com at MOEDICT 萌典松 2015/3/28
     ids=ids.substring(1,ids.length);
     var i=1;
     while (operand>0) {
-		  op=getOperandByIDC(ids.charCodeAt(0));
-		  var f=framebypart(idc,frame,i-1);
-      var child=parent["p"+i]={"ch":ids[0]}; // create a new child
-		  if (op>0) {//IDC
-		    ids=addchild(ids,child,f);
-		    fitparts(child, f);
-      } else { //normal characters
-        if (glypheme[ids[0]]) { //it is a part
-
+	op=getOperandByIDC(ids.charCodeAt(0));
+	var f=framebypart(idc,frame,i-1);
+	// 產生一個中間代號
+        var child=parent["p"+i]={"ch":ids[0]}; 
+	if (op>0) {//IDC
+	  ids=addchild(ids,child,f);
+	  fitparts(child, f);
+      	} else { //normal characters
+          if (glypheme[ids[0]]) { //it is a part
+            //這裡有點問題
         } else { //try IDS
-          childids=decompose[ids[0]];
-          ids=addchild(childids,child,f);
+          childids=decompose[ids[0]]; //看看這個部件是否有 IDS
+          ids=addchild(childids,child,f); //遞迴組字
         }
 
         ids=ids.substring(1,ids.length); // consume first char
         child.frame=f;
-
-      }
-		  i++;operand--;
+	}
+		i++;operand--;
 	  }
 	  return ids;
   }
